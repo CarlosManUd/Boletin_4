63ed0c9b68f75aee9ac20ab95e431d635530b14a
cfigueirasvalverde <cfigueirasvalverde@nautilus8.ad.danielcastelao.org>
2019-10-16 10:27:45 +0200

    Boletin4_4

--- Boletin4_1/src/boletin4_1/Boletin4_1.java
+++ Boletin4_1/src/boletin4_1/Boletin4_1.java
@@ -12,18 +12,18 @@ public class Boletin4_1 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
 
-        Coche unCoche = new Coche();
-        System.out.println("Velocidade = " + unCoche.getVelocidade() + " km/h");
+        Coche ferrari = new Coche();
+        System.out.println("Velocidade = " + ferrari.getVelocidade() + " km/h");
 
         System.out.print("\nCanto quere incrementar a velocidade = ");
-        unCoche.acelerar(ler.nextInt());
+        ferrari.acelerar(ler.nextInt());
 
-        System.out.println("Velocidade = " + unCoche.getVelocidade() + " km/h");
+        System.out.println("Velocidade = " + ferrari.getVelocidade() + " km/h");
 
         System.out.print("\nCanto quere frenar a velocidade = ");
-        unCoche.frenar(ler.nextInt());
+        ferrari.frenar(ler.nextInt());
 
-        System.out.println("Velocidade = " + unCoche.getVelocidade() + " km/h");
+        System.out.println("Velocidade = " + ferrari.getVelocidade() + " km/h");
         
     }
     
