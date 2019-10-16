59746f5615972af948b757eb27d28f8585981ebe
cfigueirasvalverde <cfigueirasvalverde@nautilus8.ad.danielcastelao.org>
2019-10-16 09:21:54 +0200

    Boletin4_4

---
+++ Boletin4_4/src/boletin4_4/Boletin4_4.java
@@ -0,0 +1,44 @@
+package boletin4_4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin4_4 {
+
+    
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Polbo restaurante = new Polbo(2, 3);
+        Boolean r;
+        Boolean s = false;
+
+        while (s == false) {
+            restaurante.amosarPolbo();
+            restaurante.amosarPatatas();
+
+            System.out.print("Pueden comer: " + restaurante.clientes()
+                    + " clientes");
+            System.out.println("\nComen?(true/false): ");
+            r = ler.nextBoolean();
+
+            if (r == true) {
+                restaurante.comen();
+            }
+            restaurante.amosarPolbo();
+            restaurante.amosarPatatas();
+
+            System.out.print("\nCuantos kg de Polbo engade?: ");
+            restaurante.engadirPolbo(ler.nextInt());
+            System.out.print("Cuantos kg de Patatas engade?: ");
+            restaurante.engadirPatatas(ler.nextInt());
+
+            System.out.println("Quiere salir?(true/false): ");
+            s = ler.nextBoolean();
+        }
+
+       
+}
+}
