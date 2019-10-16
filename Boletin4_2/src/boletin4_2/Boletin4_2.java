bde6c0866f93c7fbbb9a4aaf9237a180a565ad78
cfigueirasvalverde <cfigueirasvalverde@nautilus8.ad.danielcastelao.org>
2019-10-16 10:39:43 +0200

    Boletin4_2

--- Boletin4_2/src/boletin4_2/Boletin4_2.java
+++ Boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -1,5 +1,7 @@
 package boletin4_2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author cfigueirasvalverde
@@ -8,11 +10,21 @@ public class Boletin4_2 {
 
     
     public static void main(String[] args) {
-         Satelite unSatelite = new Satelite(10, 25, 4);
+        Satelite unSatelite = new Satelite(10, 25, 4);
         unSatelite.verPosicion();
 
         Satelite outroSatelite = new Satelite();
         outroSatelite.verPosicion();
+
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Valor meridiano = ");
+       double meridiano=sc.nextDouble();
+       System.out.println("Valor paralelo= ");
+       double paralelo= sc.nextDouble();
+       System.out.println("Valor distancia Terra= ");
+       double distanciaTerra= sc.nextDouble();
+       Satelite sat3= new Satelite(meridiano,paralelo,distanciaTerra);
+       sat3.verPosicion();
 
         
     }
